43bb7e89c1839b8167073cd09ded37372d94779a
Marco <mclarenmarco998@gmail.com>
2023-03-08 19:10:38 +0200

    Add project files.

---
+++ WebAPI/Data/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+      : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
