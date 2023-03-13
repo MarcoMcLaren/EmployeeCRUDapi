c7cb3a63203ca1bd78096c1a15a591f49b426901
Marco McLaren <mclarenmarco998@gmail.com>
2023-03-13 18:18:24 +0200

    GetAllEmployees and AddEmployees works perfectly!!

---
+++ WebAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly MyDbContext _db;
+        public EmployeesController(MyDbContext mydbcontext)
+        {
+           _db = mydbcontext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees = await _db.Employees.ToListAsync();
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee([FromBody] Employee employeeRequest)
+        {
+            employeeRequest.Id = Guid.NewGuid();
+            await _db.Employees.AddAsync(employeeRequest);
+            await _db.SaveChangesAsync();
+            return Ok(employeeRequest);
+        }
+    }
+}
